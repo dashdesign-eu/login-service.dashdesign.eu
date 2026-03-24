9fec078aaa37f17e3929eaf39a1d1b504fadc6a4
Konrad Mayer <64093101+konipro@users.noreply.github.com>
2026-03-24 20:12:14 +0100

    Update authViews.js

--- src/views/authViews.js
+++ src/views/authViews.js
@@ -31,7 +31,7 @@ export function renderPortalHtml({ returnTo = '' } = {}) {
   <div class="wrap">
     <h2>dashdesign Login</h2>
     <div class="card">
-      <p class="muted">Melde dich mit deinem dashdesign Account an.</p>
+      <p class="muted">Melde dich mit deinem dashdesign; Account an.</p>
       <form id="f">
         <label>Benutzername oder E-Mail</label><br/>
         <input required type="text" id="u" name="username" autocomplete="username" autocorrect="off" autocapitalize="none" spellcheck="false"/><br/><br/>
